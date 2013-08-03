1fa94c8ba94a7d861f54b11143d958b0b9d20f3a
Saket Khandelwal <kanpur.saket@gmail.com>
2013-08-03 11:38:07 +0000

    My html updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+buffer.write(fs.readFileSync('index.html'),"utf-8");  
+response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
