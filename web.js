1d07609b8cb10fc85c51c01b376dd5ea8a4a3fc8
Saket Khandelwal <kanpur.saket@gmail.com>
2013-08-03 12:25:54 +0000

    My html7 updated web.js

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var htmlfile = "index.html"
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-buf=new Buffer(256);
-var content = fs.readFileSync('/bitstarter/index.html');
-buf.write(content);
-response.send(buf.toString('utf8'));
+//buf=new Buffer(256);
+var content = fs.readFileSync(htmlfile).toString();
+//buf.write(content);
+response.send(content);
 });
 
 var port = process.env.PORT || 5000;
